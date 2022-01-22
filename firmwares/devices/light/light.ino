40a365aa8b8628a604fb43527ec9f292b5247667
Dmitriy Krylov <dmitriy.krylov@akvelon.com>
2022-01-22 11:56:22 +0300

    light firmware

--- firmwares/devices/light/light.ino
+++ firmwares/devices/light/light.ino
@@ -1,44 +1,58 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+
+// Pins
+static const uint8_t D0   = 16;
+static const uint8_t D1   = 5;
+static const uint8_t D2   = 4;
+static const uint8_t D3   = 0;
+static const uint8_t D4   = 2;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 15;
+static const uint8_t D9   = 3;
+static const uint8_t D10  = 1;
  
 // Settings
 const char* ssid = "KHOME";
 const char* password = "q12345";
-const char* deviceId = "room1-device1";
+const char* deviceId = "room1-device3";
 const char* mqttHost = "192.168.0.5";
 const int mqttPort = 1883;
-const char* mqttTopic = "61d2bfd71d3d6e69f6f53141-LIGHT";
+const char* mqttTopic = "61d2bfd71d3d6e69f6f53141-LIGHT_DEVICE";
 const byte ledPin = LED_BUILTIN;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  DynamicJsonDocument doc(1024);
-  auto error = deserializeJson(doc, payload);
-  if (error) {
-    Serial.print(F("deserializeJson() failed with code "));
-    Serial.println(error.c_str());
-    return;
+ 
+void loop() {
+  if (!client.connected()) {
+    reconnect();
   }
-  const char* deviceId = doc["deviceId"];
-  const bool state = doc["state"];
-  
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-
-  if(state) {
-    digitalWrite(ledPin, LOW);
-    Serial.print("state true");
-  } else {
-    digitalWrite(ledPin, HIGH);
-    Serial.print("state false");
-  }
-  Serial.println();
+  client.loop();
 }
  
+void setup() {
+  Serial.begin(9600);
+  client.setServer(mqttHost, mqttPort);
+  client.setCallback(callback);
+  pinMode(D2, OUTPUT);
+  connectToWifi();
+}
+
+void connectToWifi() {
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
  
 void reconnect() {
   // Loop until we're reconnected
@@ -59,29 +73,27 @@ void reconnect() {
   }
 }
 
-void connectToWifi() {
-  WiFi.begin(ssid, password);
+void callback(char* topic, byte* payload, unsigned int length) {
+  DynamicJsonDocument doc(1024);
+  auto error = deserializeJson(doc, payload);
+  if (error) {
+    Serial.print(F("deserializeJson() failed with code "));
+    Serial.println(error.c_str());
+    return;
+  }
+  const char* deviceId = doc["deviceId"];
+  const bool state = doc["state"];
+  
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
 
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  if(state) {
+    digitalWrite(D2, LOW);
+    Serial.print("state true");
+  } else {
+    digitalWrite(D2, HIGH);
+    Serial.print("state false");
   }
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-}
- 
-void setup() {
-  Serial.begin(9600);
-  client.setServer(mqttHost, mqttPort);
-  client.setCallback(callback);
-  pinMode(ledPin, OUTPUT);
-  connectToWifi();
-}
- 
-void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  Serial.println();
 }
