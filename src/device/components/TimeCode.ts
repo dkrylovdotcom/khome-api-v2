86081715dca6f5ce8a7dd323972170bc6179dfeb
Dmitriy Krylov <dmitriy.krylov@akvelon.com>
2022-07-30 18:20:10 +0300

    feat: timecode component updated

--- src/device/components/TimeCode.ts
+++ src/device/components/TimeCode.ts
@@ -1,43 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import md5 from 'md5';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const totp = require('totp-generator');
 
 @Injectable()
 export class TimeCode {
-  constructor(private readonly configService: ConfigService) {}
+  private readonly timeCodeLength: number;
+  private readonly timeCodeInterval: number;
+  // TODO: possible, it should be different for each user
+  private readonly key = 'JBSWY3DPEHPK3PXP';
+
+  constructor(private readonly configService: ConfigService) {
+    this.timeCodeLength = this.configService.get('TIMECODE_LENGTH') as number;
+    this.timeCodeInterval = this.configService.get('TIMECODE_INTERVAL') as number;
+  }
 
   public getCode() {
-    const timeCodeInterval = this.configService.get('TIMECODE_INTERVAL');
-    const timeCodeLength = this.configService.get('TIMECODE_LENGTH');
-    const timestamp = Date.now();
-    const intervalStarted = this.nok(timestamp, timeCodeInterval);
-    const intervalEnd = this.nod([timestamp, timeCodeInterval]);
-
-    if (timestamp > intervalStarted && timestamp < intervalEnd) {
-      const hashedString = md5(intervalEnd.toString());
-      const numbers = parseInt(hashedString).toFixed(timeCodeLength);
-      return numbers;
-    }
-  }
-
-  private nod(A: any) {
-    const n = A.length;
-    let x = Math.abs(A[0]);
-    for (let i = 1; i < n; i++) {
-      let y = Math.abs(A[i]);
-      while (x && y) {
-        x > y ? (x %= y) : (y %= x);
-      }
-      x += y;
-    }
-    return x;
-  }
-
-  private gcd(n: any, m: any): any {
-    return m == 0 ? n : this.gcd(m, n % m);
-  }
-
-  private nok(n: any, m: any) {
-    return (n * m) / this.gcd(n, m);
+    return totp(this.key, {
+      digits: this.timeCodeLength,
+      period: this.timeCodeInterval,
+    });
   }
 }
